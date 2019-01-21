081b87798fe3812a4fda5c5299a92858c4a63554
PrerakSheth <preraksheth55@gmail.com>
2019-01-21 14:18:24 +0530

    21-1
    sd

--- PatchPets Latest/app/src/main/java/com/patchpets/webservices/ResponseCallback.java
+++
@@ -1,7 +0,0 @@
-package com.patchpets.webservices;
-
-public interface ResponseCallback {
-    public void ResponseSuccessCallBack(Object object);
-
-    public void ResponseFailCallBack(Object object);
-}
